af5415b593e9de0bf83bb041d04868a1f89a4b68
Magalie Linda <magaliechetrit@gmail.com>
2020-10-20 21:45:10 +0200

    Removed bootstrap, added my own styling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'apple-book-converter';
+  result: number;
+  enteredNumberAB: string;
+  enteredNumberBook: string;
+  showResult: false;
+  onInputAB(value: string) {
+    // console.log(value);
+    this.enteredNumberAB = value
+    /*
+    if (value === this.randomtText) {
+      this.solved = true;
+    }
+    */
+  }
+  onInputBook(value: string) {
+    // console.log(value);
+    this.enteredNumberBook = value
+    /*
+    if (value === this.randomtText) {
+      this.solved = true;
+    }
+    */
+  }
+
+  calculateResult() {
+    const abCurrent = parseInt(this.enteredNumberAB);
+    const bookTotalPages = parseInt(this.enteredNumberBook);
+    let pageCount = Math.round(bookTotalPages / 100 * abCurrent);
+    console.log(pageCount);
+    this.result = pageCount;
+  }
 }
+
